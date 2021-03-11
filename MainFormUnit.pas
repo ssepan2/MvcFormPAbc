a7972d7e514d50891581b1c9d89fe02b3a87317c
Stephen Sepan <sjsepan@outlook.com>
2021-03-11 07:56:25 -0500

    initial commit

---
+++ MainFormUnit.pas
@@ -0,0 +1,45 @@
+﻿Unit MainFormUnit;
+
+interface
+
+uses System, System.Drawing, System.Windows.Forms;
+
+type
+  MainForm = class(Form)
+  {$region FormDesigner}
+  internal
+    {$resource MainFormUnit.MainForm.resources}
+    menuStrip1: MenuStrip;
+    fileToolStripMenuItem: ToolStripMenuItem;
+    newToolStripMenuItem: ToolStripMenuItem;
+    openToolStripMenuItem: ToolStripMenuItem;
+    saveToolStripMenuItem: ToolStripMenuItem;
+    saveAsToolStripMenuItem: ToolStripMenuItem;
+    toolStripMenuItem1: ToolStripSeparator;
+    printToolStripMenuItem: ToolStripMenuItem;
+    printSetupToolStripMenuItem: ToolStripMenuItem;
+    toolStripMenuItem2: ToolStripSeparator;
+    exitToolStripMenuItem: ToolStripMenuItem;
+    editToolStripMenuItem: ToolStripMenuItem;
+    windowToolStripMenuItem: ToolStripMenuItem;
+    toolStrip1: ToolStrip;
+    toolStripButton1: ToolStripButton;
+    statusStrip1: StatusStrip;
+    toolStripStatusLabel1: ToolStripStatusLabel;
+    toolStripStatusLabel2: ToolStripStatusLabel;
+    toolStripProgressBar1: ToolStripProgressBar;
+    toolStripDropDownButton1: ToolStripDropDownButton;
+    toolStripDropDownButton2: ToolStripDropDownButton;
+    helpToolStripMenuItem: ToolStripMenuItem;
+    {$include MainFormUnit.MainForm.inc}
+  {$endregion FormDesigner}
+  public
+    constructor;
+    begin
+      InitializeComponent;
+    end;
+  end;
+
+implementation
+
+end.
---
+++ MvcFormPAbc.pas
@@ -0,0 +1,7 @@
+﻿uses MainFormUnit;
+
+begin
+  System.Windows.Forms.Application.EnableVisualStyles();
+  System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+  System.Windows.Forms.Application.Run(new MainForm)
+end.
