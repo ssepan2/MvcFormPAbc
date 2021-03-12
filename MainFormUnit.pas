2989d1a1fab710e7c31cadaccbd194539fafa254
Stephen Sepan <sjsepan@outlook.com>
2021-03-11 21:10:43 -0500

    menu, toolbar, and statusbar controls added

--- MainFormUnit.pas
+++ MainFormUnit.pas
@@ -6,6 +6,7 @@ uses System, System.Drawing, System.Windows.Forms;
 
 type
   MainForm = class(Form)
+    procedure pasteSepcialToolStripMenuItem_Click(sender: Object; e: EventArgs);
   {$region FormDesigner}
   internal
     {$resource MainFormUnit.MainForm.resources}
@@ -25,11 +26,59 @@ type
     toolStrip1: ToolStrip;
     toolStripButton1: ToolStripButton;
     statusStrip1: StatusStrip;
-    toolStripStatusLabel1: ToolStripStatusLabel;
-    toolStripStatusLabel2: ToolStripStatusLabel;
-    toolStripProgressBar1: ToolStripProgressBar;
-    toolStripDropDownButton1: ToolStripDropDownButton;
-    toolStripDropDownButton2: ToolStripDropDownButton;
+    lblStatus: ToolStripStatusLabel;
+    lblError: ToolStripStatusLabel;
+    prgProgress: ToolStripProgressBar;
+    btnAction: ToolStripDropDownButton;
+    btnDirty: ToolStripDropDownButton;
+    undoToolStripMenuItem: ToolStripMenuItem;
+    redoToolStripMenuItem: ToolStripMenuItem;
+    toolStripMenuItem3: ToolStripSeparator;
+    toolStripMenuItem4: ToolStripMenuItem;
+    cutToolStripMenuItem: ToolStripMenuItem;
+    copyToolStripMenuItem: ToolStripMenuItem;
+    pasteToolStripMenuItem: ToolStripMenuItem;
+    pasteSepcialToolStripMenuItem: ToolStripMenuItem;
+    deleteToolStripMenuItem: ToolStripMenuItem;
+    toolStripMenuItem5: ToolStripSeparator;
+    findToolStripMenuItem: ToolStripMenuItem;
+    replaceToolStripMenuItem: ToolStripMenuItem;
+    gotoToolStripMenuItem: ToolStripMenuItem;
+    toolStripMenuItem6: ToolStripSeparator;
+    refreshToolStripMenuItem: ToolStripMenuItem;
+    toolStripMenuItem7: ToolStripSeparator;
+    propertiesToolStripMenuItem: ToolStripMenuItem;
+    preferencesToolStripMenuItem: ToolStripMenuItem;
+    newWindowToolStripMenuItem: ToolStripMenuItem;
+    tileToolStripMenuItem: ToolStripMenuItem;
+    cascadeToolStripMenuItem: ToolStripMenuItem;
+    arrangeAllToolStripMenuItem: ToolStripMenuItem;
+    toolStripMenuItem8: ToolStripSeparator;
+    hideToolStripMenuItem: ToolStripMenuItem;
+    showToolStripMenuItem: ToolStripMenuItem;
+    helpContentsToolStripMenuItem: ToolStripMenuItem;
+    helpIndexToolStripMenuItem: ToolStripMenuItem;
+    helpOnHelpToolStripMenuItem: ToolStripMenuItem;
+    toolStripMenuItem10: ToolStripSeparator;
+    licenceInformationToolStripMenuItem: ToolStripMenuItem;
+    checkForUpdatesToolStripMenuItem: ToolStripMenuItem;
+    toolStripMenuItem9: ToolStripSeparator;
+    aboutToolStripMenuItem: ToolStripMenuItem;
+    toolStripButton2: ToolStripButton;
+    toolStripButton3: ToolStripButton;
+    toolStripButton4: ToolStripButton;
+    toolStripSeparator1: ToolStripSeparator;
+    toolStripButton5: ToolStripButton;
+    toolStripButton6: ToolStripButton;
+    toolStripButton7: ToolStripButton;
+    toolStripButton8: ToolStripButton;
+    toolStripButton9: ToolStripButton;
+    toolStripButton10: ToolStripButton;
+    toolStripButton11: ToolStripButton;
+    toolStripButton12: ToolStripButton;
+    toolStripButton13: ToolStripButton;
+    toolStripSeparator2: ToolStripSeparator;
+    toolStripButton14: ToolStripButton;
     helpToolStripMenuItem: ToolStripMenuItem;
     {$include MainFormUnit.MainForm.inc}
   {$endregion FormDesigner}
@@ -41,5 +90,10 @@ type
   end;
 
 implementation
+
+procedure MainForm.pasteSepcialToolStripMenuItem_Click(sender: Object; e: EventArgs);
+begin
+  
+end;
 
 end.
